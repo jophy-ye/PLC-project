2e99decf9c9c3c31d933585a49b96cc6a94a0d6c
Jophy Chon Hou Ye <jophyyjh@gmail.com>
2024-08-29 16:30:13 +0800

    Added VocabMap to every existing db example

--- PLCdb/linter.py
+++ PLCdb/linter.py
@@ -1,6 +1,8 @@
 """
 To run this PLCdb linter, install `pytest` and `pyyaml`.
+
 Run `pytest linter.py` in cmd line.
+The following packages are needed: `pyyaml`, `pytest`.
 """
 
 import os
@@ -26,7 +28,7 @@ def test_yaml(load_file):
 @pytest.mark.parametrize("src_name", src_filenames)
 def test_structure(load_file: dict):
     keys = load_file.keys()
-    INCLUDES = ('Name', 'Update', 'Input', 'Variable', 'Output', 'Rungs')
+    INCLUDES = ('Name', 'Update', 'Input', 'Variable', 'Output', 'Rungs', 'VocabMap')
     # TODO: add 'CNF'
     for include in INCLUDES:
         assert include in keys
@@ -35,3 +37,12 @@ def test_structure(load_file: dict):
 @pytest.mark.parametrize("src_name", src_filenames)
 def test_variables(load_file: dict):
     pass
+
+@pytest.mark.parametrize("src_name", src_filenames)
+def test_vocab_map(load_file: dict):
+    with open('vocab.txt', 'r') as v:
+        vocab_list = v.read().split()
+    for vocab in vocab_list:
+        assert type(vocab) is str and ' ' not in vocab
+    for key, value in load_file['VocabMap'].items():
+        assert value in vocab_list
